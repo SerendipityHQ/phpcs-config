0a3b90d23825bc903ce92ba22a56108259dc3661
Aerendir <hello@aerendir.me>
2022-07-19 21:29:58 +0200

    Update all rules.

--- src/Rules/symfony-bundle.php
+++ src/Rules/symfony-bundle.php
@@ -1,33 +1,64 @@
 <?php
 
 return [
-    'header_comment' => ['header' => $header],
-    '@Symfony' => true,
-
-    // Symfony overwritings
-    'binary_operator_spaces' => [
+    '@Symfony'                               => true,
+    'align_multiline_comment'                => true,
+    'array_syntax'                           => [
+        'syntax' => 'short',
+    ],
+    'binary_operator_spaces'                 => [
         'default' => 'align',
     ],
-    'concat_space' => ['spacing' => 'one'],
-    'phpdoc_to_comment' => false,
-
-    // Other rules
-    'align_multiline_comment' => true,
-    'array_syntax' => ['syntax' => 'short'],
-    'blank_line_before_statement' => true,
-    'combine_consecutive_unsets' => true,
-    'linebreak_after_opening_tag' => true,
-    'list_syntax' => ['syntax' => 'short'],
-    'multiline_whitespace_before_semicolons' => true,
-    'no_null_property_initialization' => true,
-    'echo_tag_syntax' => true,
-    'no_superfluous_phpdoc_tags' => false,
-    'no_unreachable_default_argument_value' => true,
-    'no_useless_else' => true,
-    'no_useless_return' => true,
-    'not_operator_with_space' => true,
-    'not_operator_with_successor_space' => true,
-    'ordered_class_elements' => [
+    'blank_line_before_statement'            => true,
+    'class_attributes_separation'            => [
+        'elements' => [
+            [
+                'const' => 'none',
+                'method' => 'one',
+                'property' => 'none',
+                'trait_import' => 'none',
+                'case' => 'none',
+            ],
+        ],
+    ],
+    'combine_consecutive_unsets'             => true,
+    'concat_space'                           => [
+        'spacing' => 'one',
+    ],
+    'declare_strict_types'                   => true,
+    'echo_tag_syntax'                        => true,
+    'fully_qualified_strict_types'           => true,
+    'general_phpdoc_annotation_remove'       => [
+        'annotations' => [
+            'throws',
+        ],
+    ],
+    'global_namespace_import'                => [
+        'import_classes' => false,
+        'import_constants' => false,
+        'import_functions' => false,
+    ],
+    'header_comment'                         => [
+        'header' => $header,
+    ],
+    'linebreak_after_opening_tag'            => true,
+    'list_syntax'                            => [
+        'syntax' => 'short',
+    ],
+    'multiline_whitespace_before_semicolons' => false,
+    'no_null_property_initialization'        => true,
+    'no_superfluous_phpdoc_tags'             => [
+        'allow_mixed' => false,
+        'remove_inheritdoc' => true,
+        'allow_unused_params' => false,
+    ],
+    'no_unreachable_default_argument_value'  => true,
+    'no_unused_imports'                      => true,
+    'no_useless_else'                        => true,
+    'no_useless_return'                      => true,
+    'not_operator_with_space'                => true,
+    'not_operator_with_successor_space'      => true,
+    'ordered_class_elements'                 => [
         'order' => [
             'use_trait',
             'constant_public',
@@ -37,25 +68,34 @@ return [
             'property_protected',
             'property_private',
             'construct',
+            'magic',
             'phpunit',
             'method_public',
             'method_protected',
             'method_private',
             'destruct',
-            'magic'
-        ]
+        ],
     ],
-    'ordered_imports' => true,
-    'phpdoc_add_missing_param_annotation' => true,
-    'phpdoc_line_span' => [
+    'ordered_imports'                        => [
+        'sort_algorithm' => 'alpha',
+        'imports_order' => [
+            'class',
+            'function',
+            'const',
+        ],
+    ],
+    'phpdoc_add_missing_param_annotation'    => true,
+    'phpdoc_line_span'                       => [
         'const' => 'single',
         'method' => 'multi',
         'property' => 'single',
     ],
-    'phpdoc_order' => true,
-    'phpdoc_types_order' => ['null_adjustment' => 'always_last'],
-    'phpdoc_var_without_name' => false,
-    'single_line_comment_style' => ['comment_types' => ['hash']],
-    'strict_comparison' => true
+    'phpdoc_order'                           => true,
+    'phpdoc_to_comment'                      => false,
+    'phpdoc_types_order'                     => [
+        'null_adjustment' => 'always_last',
+    ],
+    'phpdoc_var_without_name'                => false,
+    'single_line_comment_style'              => true,
+    'strict_comparison'                      => true,
 ];
-
